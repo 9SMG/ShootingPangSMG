6a3b85c6f5d3b53ee45892d51100cca9d717b589
SMG9 <asnmfg@naver.com>
2025-03-14 19:30:55 +0900

    grappling hook
    원운동 구현(추가 구현 필요)

--- ShootingPangSMGRework/Assets/02.Scripts/WireTest.cs
+++ ShootingPangSMGRework/Assets/02.Scripts/WireTest.cs
@@ -7,6 +7,7 @@ public class WireTest : MonoBehaviour
     Transform mouseCursorTr;
     Rigidbody2D rb;
     DistanceJoint2D distJoint;
+    //SpringJoint2D springJoint;
 
     void Start()
     {
@@ -15,22 +16,27 @@ public class WireTest : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
         distJoint = GetComponent<DistanceJoint2D>();
+        //springJoint = GetComponent<SpringJoint2D>();
     }
 
-    
+
+    bool isClicked = false;
+    Vector3 mousePos;
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mouseCursorTr.position = new Vector3(mousePos.x, mousePos.y, 0f);
-
+        if (GameManager.Instance.GetDragable())
+            return;
+        
+        if (true)//!isClicked)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseCursorTr.position = new Vector3(mousePos.x, mousePos.y, 0f);
+        }
+        
         Vector3 dir = mousePos - transform.position;
         dir.z = 0f;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 100f, LayerMask.GetMask("Wall"));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, Mathf.Infinity, LayerMask.GetMask("Wall"));
         lineRenderer.SetPosition(1, transform.InverseTransformPoint(hit.point));
-        //raycastHitPointTr.transform.position = transform.InverseTransformPoint(hit.point);
-        //Debug.Log(hit.point);
-        //distJoint.connectedAnchor = hit.point;
-
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -39,7 +45,44 @@ public class WireTest : MonoBehaviour
         float speed = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector2.right * speed * 9f * Time.deltaTime);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            distJoint.enabled = true;
+            distJoint.connectedAnchor = hit.point;
+            distJoint.distance = Vector2.Distance(transform.position, hit.point);
+
+            //springJoint.enabled = true;
+            //springJoint.connectedAnchor = hit.point;
+            //springJoint.distance = Vector2.Distance(transform.position, hit.point);
+
+            lineRenderer.enabled = false;
+
+            //rb.linearVelocity *= dir.normalized * hookPower;
+            //Debug.Log((dir.normalized * hookPower));
+            //transform.parent.GetComponent<Rigidbody2D>().linearVelocity = transform.parent.GetComponent<Rigidbody2D>().linearVelocity + (Vector2)(dir.normalized * hookPower);
+            //rb.mass = 1f;
+            //rb.linearDamping = 0.1f;
+            //lineRenderer.startColor = new Color(0.5f, 0.5f, 0.5f);
+            //lineRenderer.endColor = new Color(0.5f, 0.5f, 0.5f);
+            isClicked = true;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            distJoint.enabled = false;
+            //springJoint.enabled = false;
+
+            lineRenderer.enabled = true;
+            //rb.mass = 2f;
+            //rb.linearDamping = 0.5f;
+            //lineRenderer.startColor = new Color(1f, 1f, 1f);
+            //lineRenderer.endColor= new Color(0f, 0.92f, 1f);
+            isClicked = false;
+        }
+
     }
+
+    public float hookPower = 2f;
 
 
 }
